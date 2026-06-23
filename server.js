bac884fb9121c1cea8b7417dffec9783ba33f4cd
yachttracker <ph@heinzels.de>
2026-06-23 21:35:49 +0200

    Update server.js

--- server.js
+++ server.js
@@ -235,7 +235,7 @@ app.get('/debug/coverage-test/status', (req, res) => {
 const PORT = process.env.PORT || 3000;
 
 setupDB().then(() => {
-  connectAIS();
+ // connectAIS();
   app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 });
   
